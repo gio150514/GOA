56894341965d7f078a62257c0360a64fee664507
gio150514 <gioalashvili150514@gmail.com>
2026-03-05 18:27:55 +0400

    commit

---
+++ level08/homework/hw.py
@@ -0,0 +1,7 @@
+#input მომხმარებელს აძლევს ნებას ჩაწეროს 
+name=input('enter your name')   
+name=input('enter your age') 
+name=input('enter your surname')  
+print=('enter your age') 
+print=('enter your name')   
+print=('enter your surname') 
